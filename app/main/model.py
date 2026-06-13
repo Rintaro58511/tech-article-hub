23abe66f90db4b282cb64910f94069a5fc88fb4f
rintao58511 <rintao1453@gmail.com>
2026-06-14 00:26:34 +0900

    feature: 日付の追加とページへのリンク追加

--- app/main/model.py
+++ app/main/model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -9,3 +10,5 @@ class Favorite(db.Model):
     title = db.Column(db.String(500))
 
     url = db.Column(db.String, unique = True)
+
+    regist_date = db.Column(db.DateTime, default = datetime.now)
